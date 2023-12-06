b77df4ca8a84dd20dc0ebe21120c838bdfd218f6
Eddie <callenberg99@gmail.com>
2023-12-06 09:43:00 +0100

    Gjort Class och Interface för Contact

---
+++ Adressbok/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using Adressbok.Interfaces;
+
+namespace Adressbok.Models
+{
+    public class Contact : IContact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public Contact (string firstName, string lastName, string phoneNumber, string email, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Address = address;
+        }
+    }
+}
